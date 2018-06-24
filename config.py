0515c6c7f4dce4bf5f5058d3ee1c8fbbac70b819
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2018-06-24 14:44:12 -0400

    Fixed linux support and modified how user is polled.

--- config.py
+++ config.py
@@ -1,18 +1,23 @@
-from os import getlogin
+from os import getlogin, getuid
 from sys import platform
 from pathlib import Path
 
 
 # DT Format for log.
 DT_FORMAT = "%Y%m%d_%H%M%S"
-# Username used for log location.
-USER = getlogin()
 
 # Path of home.
-if platform is 'win32':
+if platform == 'win32':
+    # Username used for log location.
+    USER = getlogin()
     PATH_HOME = Path(r'C:\Users\{0}\Desktop'.format(USER))
-elif platform is 'linux' or platform is 'darwin':
+elif platform == 'linux' or platform == 'darwin':
+    import pwd
+    USER = pwd.getpwuid(getuid())[0]
     PATH_HOME = Path(r'/home/{0}/'.format(USER))
+else:
+    print(platform)
+    exit(5)
 
 # Folder where data is stored.
 FOLDER = r'logs'
