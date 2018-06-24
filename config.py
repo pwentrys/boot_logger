1fcd83c1fefdbd9c82f7a0005bf5da13f49dc93e
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2018-06-24 14:28:05 -0400

    Added os specific check for home path.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os import getlogin
+from sys import platform
 from pathlib import Path
 
 
@@ -6,8 +7,13 @@ from pathlib import Path
 DT_FORMAT = "%Y%m%d_%H%M%S"
 # Username used for log location.
 USER = getlogin()
+
 # Path of home.
-PATH_HOME = Path(r'C:\Users\{0}\Desktop'.format(USER))
+if platform is 'win32':
+    PATH_HOME = Path(r'C:\Users\{0}\Desktop'.format(USER))
+elif platform is 'linux' or platform is 'darwin':
+    PATH_HOME = Path(r'/home/{0}/'.format(USER))
+
 # Folder where data is stored.
 FOLDER = r'logs'
 # File where log is written to.
