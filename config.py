f888d3583d6d57abffd08f0af3af9b7eb34d19b2
Przemyslaw "Blasto" Wentrys <pwentrys@gmail.com>
2018-06-24 14:21:49 -0400

    Added config for Windows env.

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ from os import getlogin
 from pathlib import Path
 
 
+# DT Format for log.
+DT_FORMAT = "%Y%m%d_%H%M%S"
 # Username used for log location.
 USER = getlogin()
 # Path of home.
-PATH = Path(r'C:\Users\{0}\Desktop'.format(USER))
+PATH_HOME = Path(r'C:\Users\{0}\Desktop'.format(USER))
 # Folder where data is stored.
 FOLDER = r'logs'
 # File where log is written to.
